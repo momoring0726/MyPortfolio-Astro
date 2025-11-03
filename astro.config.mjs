682f9ba76e2c34b45a986d84e3c20a92bc11dcbc
Mark Wyndl Tan Divinagracia <markwyndltan@gmail.com>
2025-11-03 08:39:35 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite"; // Import the plugin
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // It goes in the 'vite' object, not 'integrations'
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
